fdd4c124d46539742793abf26e1e96e4e4701f51
alliegriffinn <80468156+alliegriffinn@users.noreply.github.com>
2022-05-10 10:17:33 -0300

    Update me.go

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/alliegriffinn/clirescue/cmdutil"
+	"github.com/alliegriffinn/clirescue/user"
 )
 
 var (
